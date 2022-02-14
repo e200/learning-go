2023c5ad21bf727c2626b0eb9b1487af8cb02e7e
Eleandro Duzentos <eleandro@inbox.ru>
2022-02-14 17:54:31 +0000

    refactor: converted tabs to spaces

--- structs.go
+++ structs.go
@@ -3,34 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	person := person{
-		firstName: "Eleandro",
-		lastName: "Duzentos",
-	}
-	
-	fmt.Println(person.getFullName())
-	
-	person.printLifeStatus()
-	
-	person.kill()
-	
-	person.printLifeStatus()
+  person := person{
+    firstName: "Eleandro",
+    lastName: "Duzentos",
+  }
+  
+  fmt.Println(person.getFullName())
+  
+  person.printLifeStatus()
+  
+  person.kill()
+  
+  person.printLifeStatus()
 }
 
 type person struct {
-	firstName string
-	lastName string
-	isDead bool
+  firstName string
+  lastName string
+  isDead bool
 }
 
 func (p person) getFullName() string {
-	return p.firstName + " " + p.lastName;
+  return p.firstName + " " + p.lastName;
 }
 
 func (p *person) kill() {
-	p.isDead = true
+  p.isDead = true
 }
 
 func (p person) printLifeStatus() {
-	fmt.Println(p.isDead)
+  fmt.Println(p.isDead)
 }
